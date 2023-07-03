5a979acc4f85e4dab84340d03e9f8b3d8ec00976
BenjaminClaro <benjamin.claro@free.fr>
2023-07-03 10:36:20 +0200

    new file:   TBDEV23040/JavaScript/Phase js 06/index.html 	new file:   TBDEV23040/JavaScript/Phase js 06/js/javascript.js

---
+++ TBDEV23040/JavaScript/Phase js 06/js/javascript.js
@@ -0,0 +1,25 @@
+var a = "100";
+var b = 100;
+var c = 1.00;
+var d = true;
+
+
+
+document.write("Ceci est une chaine de caractères : " + a + "<br>");
+
+var b = --b;
+
+document.write(b + "<br>");
+
+var c = c + a;
+
+document.write(c+ "<br>");
+
+if (d == true){
+    var d = false;
+}
+else{
+    var d = true;
+} 
+
+document.write(d);
